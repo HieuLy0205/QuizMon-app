0cbb741d88676c4ca8a4fc125149485b52168741
nguyen phong <phonglaptopvg@hotmail.com>
2026-04-17 10:50:25 +0700

    thêm back (imagerbutten) cho trang giao diện cửa hàng, pet, tăng cone bằng text thông qua lưu trử PreferenceManager _hàm getcoin lay coin từ xml PreferenceManager tạo hàm addcoin công thức (saveCoins(current + amount) tạm thời

--- app/src/main/java/com/example/quizmon/ui/shop/shop_tim.kt
+++ app/src/main/java/com/example/quizmon/ui/shop/shop_tim.kt
@@ -1,10 +1,14 @@
 package com.example.quizmon.ui.shop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.quizmon.R
 class shop_tim: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_tim)
+
+        val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        btnBack.setOnClickListener { finish() }
     }
 }
