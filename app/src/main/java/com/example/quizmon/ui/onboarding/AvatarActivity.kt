ce738be5ff43609990bfea9660fadb9cd832fa5e
HieuLy0205 <hieult6681@ut.edu.vn>
2026-05-04 09:14:46 +0700

    +

--- app/src/main/java/com/example/quizmon/ui/onboarding/AvatarActivity.kt
+++ app/src/main/java/com/example/quizmon/ui/onboarding/AvatarActivity.kt
@@ -36,6 +36,9 @@ class AvatarActivity : AppCompatActivity() {
         val btnAvatar2 = findViewById<Button>(R.id.btnAvatar2)
         val btnAvatarVip = findViewById<Button>(R.id.btnAvatarVip)
 
+        // Định dạng lại text cho các nút Avatar
+        btnAvatar1.text = getString(R.string.avatar_item_format, 1)
+        btnAvatar2.text = getString(R.string.avatar_item_format, 2)
 
         val btnFrame1 = findViewById<Button>(R.id.btnFrame1)
         val btnFrame2 = findViewById<Button>(R.id.btnFrame2)
